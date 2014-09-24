4ded633af28cc1e8b3431a5ed48ed491281fc52d
ethirajsrinivasan <ethirajsrinivasan@gmail.com>
2014-09-24 18:26:36 +0530

    jquery-ui in action

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module OTTS
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << File.join(Rails.root,'/vendor/assets/images/')
   end
 end
